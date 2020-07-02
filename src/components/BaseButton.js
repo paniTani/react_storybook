56a6a722435b27277bcfc5f9395f8471807ea0d7
Tania Miahkoholova <tatianamiahkoholova@gmail.com>
2020-07-02 10:23:06 +0300

    add addon-info plugin

--- src/components/BaseButton.js
+++ src/components/BaseButton.js
@@ -5,7 +5,7 @@ const BaseButton = styled.button`
   border: ${props => props.yellow ? 'none' : '1px solid var(--lightBlack)' };
   background-color: ${props => props.yellow ? "var(--yellow)" : "transparent"};
   height: ${props => props.high ? "50px" : "37px"} ;
-  border-radius: 4px;
+  border-radius: ${props => props.square ? "0px" : "4px"};
   font-weight: 600;
   padding: 0 15px;
   transition: opacity 0.2s;
--- src/components/stories/Button.stories.js
+++ src/components/stories/Button.stories.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 import BaseButton from '../BaseButton.js';
 import '../../index.css';
 
 export default {
   title: 'Button',
   component: BaseButton,
+  decorators: [withInfo],
+  parameters: {
+    info: {
+      text: ` description or documentation about my component, supports markdown
+
+    ~~~js
+    <Button>Click Here</Button>
+    ~~~
+  `,
+    },
+  },
 };
 
 export const BaseBtn = () => <BaseButton onClick={action('clicked')}>Custom</BaseButton>;
 export const BaseYellowButton = () => <BaseButton onClick={action('clicked')} yellow>Custom(yellow)</BaseButton>;
+export const SquareButton = () => <BaseButton square>Custom(square)</BaseButton>;
